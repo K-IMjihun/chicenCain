48cf1dc854e7d166b2afead83ed18f953848c8cd
K-IMjihun <62210749+K-IMjihun@users.noreply.github.com>
2022-12-16 13:00:45 +0900

    Payment // 코드수정

--- src/Payment.java
+++ src/Payment.java
@@ -1,23 +1,26 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Payment {
   static Output output = new Output();
   static Order order = new Order();
+  static String table = "";
   public static void payment(){
     output.Output();
     Scanner sc = new Scanner(System.in);
     System.out.println("## 테이블을 선택하세요.");
-    String table = sc.next();
+    table = sc.next();
     tableCheck(table);
     tablepay(table);
   }
   public static void tableCheck(String table){
-    if(!order.getOrderTable().contains(table)){
+    if(!order.getTablePrice().containsKey(table)){
       throw new IllegalArgumentException("ERROR : 올바르지 않은 테이블 번호입니다");
     }
   }
   public static void tablepay(String table){
     System.out.println("## 최종 결제할 금액");
-    System.out.println(order.getTablePrice()[Integer.parseInt(table)-1] + "원\n");
+    System.out.println(order.getTablePrice().get(table) + "원\n");
+    order.paymentCompleted(table);
   }
 }
